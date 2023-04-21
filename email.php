f46150c02f43cb5a2593c48218e3f4330c0f042b
Marks Henrik <markshenrik13@gmail.com>
2023-04-21 10:37:39 -0300

    Modificando formulário 2

--- email.php
+++ email.php
@@ -1,36 +1,36 @@
 <?php
   //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+  // $nome = $_POST['nome'];
+  // $email = $_POST['email'];
+  // $mensagem = $_POST['mensagem'];
+  // $data_envio = date('d/m/Y');
+  // $hora_envio = date('H:i:s');
 
   //Campo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
+  // $arquivo = "
+  //   <html>
+  //     <p><b>Nome: </b>$nome</p>
+  //     <p><b>E-mail: </b>$email</p>
+  //     <p><b>Mensagem: </b>$mensagem</p>
+  //     <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+  //   </html>
+  // ";
   
   //Emails para quem será enviado o formulário
-  $destino = "markshenrik13@gmail.com";
-  $assunto = "Contato pelo Portfólio";
+  // $destino = "markshenrik13@gmail.com";
+  // $assunto = "Contato pelo Portfólio";
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  // $headers  = "MIME-Version: 1.0\n";
+  // $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  // $headers .= "From: $nome <$email>";
 
   //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
+  // mail($destino, $assunto, $arquivo, $headers);
   
-  if(mail($destino, $assunto, $arquivo, $headers)){
-    echo("Email enviado com sucesso!");
-  }else{
-    echo("O Email não pode ser enviado");
-  }
+  // if(mail($destino, $assunto, $arquivo, $headers)){
+  //   echo("Email enviado com sucesso!");
+  // }else{
+  //   echo("O Email não pode ser enviado");
+  // }
 ?>
